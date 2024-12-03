724f3675ca2c38eea12af9782f550837f26abb0a
Tcordeiro <talysmatheus1@gmail.com>
2024-12-03 18:50:31 -0300

    nenhuma atualização relevante

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,21 +1,18 @@
-/*Função do formulário preenchido no final da pág */
-class FormSubmit {
-    constructor(configuraçoes) {
-        this.configuraçoes = configuraçoes;
-        this.form = document.querySelector(settings.form);
-        this.formButton = document.querySelector(settings.button);
-        if (this.form) {
-            this.url = this.form.getAttribute("action");
-        }
-    }
+const script_do_google = 'https://script.google.com/macros/s/AKfycbwBgGLa0Dm65VEcri8lorQy7bNZSQHNQ3NMAGGOX278K1j8TynaC3vMVX6Zo7_ZC6B2/exec';
+const dados_do_formulario = document.forms ['formulario-contato'];
 
+dados_do_formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-displaySucces() {
-    this.form.innerHTML = this.setting.sucess;
-}
+    fetch(script_do_google, { method: 'POST', body: new FormData(dados_do_formulario) })
+        .then(response => {
+            // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
+            alert('Dados enviados com sucesso!', response);
+            dados_do_formulario.reset(); 
+        })
+        .catch(error => {
+            // Se houver erro no envio, a menssagem abaixo será exibida
+            console.error('Erro no envio dos dados!', error);
+        });
+});
 
-displayError() {
-    this.form.innerHTML = this.setting.error;
-}
-
-}
